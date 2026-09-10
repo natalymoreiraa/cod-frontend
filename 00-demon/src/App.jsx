21001a282c9ca278ae63cd373fdca62f5cca4a91
nataly <nataly_moreira@estudante.sesisenai.org.br>
2026-09-10 15:57:32 -0300

    projeto fofo

---
+++ 00-demon/src/App.jsx
@@ -0,0 +1,58 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div className="cont-app">
+    <h1>Trampando com React</h1>
+    <h2>Onde posso trampar com o dev</h2>
+  <p>Empresas de Tecnologia e Software Houses: Atuação direta no desenvolvimento de sistemas web, aplicações SaaS, e plataformas digitais para clientes diversos.</p>
+ 
+  <p>Agências Digitais e de Marketing: Criação e manutenção de sites, landing pages, e-commerces e portais interativos para marcas e clientes da agência.</p>
+  <p>Startups: Desenvolvimento ágil de produtos mínimos viáveis (MVPs), manutenção de plataformas e criação de novas funcionalidades em times enxutos.</p>
+  <p>Departamentos de TI em Grandes Empresas: Atuação no desenvolvimento e manutenção de sistemas internos (intranets, painéis administrativos) em setores como bancos, varejo, indústria e saúde.</p>
+  <p>Setor Público: Suporte e desenvolvimento de portais governamentais, sistemas de atendimento ao cidadão e transparência.</p>
+  <p>Trabalho Autônomo / Freelancer: Prestação de serviços diretos para pequenos e médios negócios (criação de sites, e-commerces e integrações de API) ou prestação de serviços por plataformas globais.</p>
+  <ul>
+    <li>Desenvolvedor Front-End: Focado na interface visual e interatividade (HTML, CSS, JavaScript, React/Vue).</li>
+    <li>Desenvolvedor Back-End: Focado na lógica do servidor, banco de dados e APIs (Node.js, PHP, Python, SQL).</li>
+    <li>Desenvolvedor Full-Stack Júnior: Atuando ponta a ponta na criação de pequenas aplicações web.</li>
+    <li>Webmaster / Mantenedor de Sites: Gestão, atualização e otimização de performance/SEO de plataformas existentes.</li>
+    <li>Desenvolvedor de E-commerce / Plataformas (ex: Shopify, WooCommerce, Magento): Focado na criação, customização e manutenção de lojas virtuais, configurando meios de pagamento, integração de frete e layout.</li>
+  
+  </ul>
+
+<h2>O que tem de trampo com tecnologia mas sem programacao?</h2>
+<p>Product Management & Negócios</p>
+<p>Product Owner (PO) / Product Manager (PM): Define o que o produto deve fazer, prioriza tarefas e faz a ponte entre os clientes e os desenvolvedores.</p>
+<p> Analista de Requisitos / Negócios: Mapeia as necessidades das empresas e as traduz em especificações técnicas para a equipe de tecnologia.</p>
+<p>Design e Experiência do Usuário (UI/UX)</p>
+<p>UX Designer: Pesquisa o comportamento do usuário e desenha fluxos de navegação para tornar os sistemas fáceis e intuitivos</p>
+
+
+<h2> vou fazer faculdade de outra coisa, ser dev vai me ajudar?</h2>
+<ul>
+  <li>Medicina e Saúde: Criação de HealthTechs, algoritmos de diagnóstico por IA e automação de prontuários médicos.</li>
+<li>Administração: Automação de processos operacionais, análise preditiva em Python e prototipagem rápida de negócios.</li>
+<li>Direito: Desenvolvimento de LegalTechs, criação de Smart Contracts e raspagem de dados processuais em massa.</li>
+<li>Design e Comunicação: Construção de interfaces interativas (UX/UI), jornalismo de dados e automação de marketing.</li>
+<li>Engenharia Civil/Arquitetura: Programação de design parametrizado, automação BIM e simulação computacional de estruturas.</li>
+</ul>
+  </div> 
+
+
+
+
+
+
+
+
+  )
+
+
+}
+
+export default App;
